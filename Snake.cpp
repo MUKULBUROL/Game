812faa9af1cf922173a417cb58df25dc93e49ea1
mukul_kumar200 <118387790+MUKULBUROL@users.noreply.github.com>
2024-07-20 23:09:42 +0530

    Update Snake.cpp
    UPDATED SNAKE KING GAME

--- Snake.cpp
+++ Snake.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
-#include<conio.h>
-#include<Windows.h>
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+#include <fstream>
+
 using namespace std;
 
-enum Direction{STOP = 0, LEFT, RIGHT, UP, DOWN};
+enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
 Direction dir;
 bool gameOver;
 const int height = 20;
@@ -12,124 +14,145 @@ int headX, headY, fruitX, fruitY, score;
 int tailx[100], taily[100];
 int tail_len;
 
+// Function declarations
 void setup();
 void draw();
 void input();
 void logic();
+void setColor(int textColor, int backgroundColor);
+void updateHighScore(int score);
 
-int main(){
+int main() {
     char start;
     cout << "\t-------------------------------" << endl;
     cout << "\t\t :Snake King:" << endl;
     cout << "\t-------------------------------" << endl;
-    cout << "\tPress 's' to option: ";
+    cout << "\tPress 's' to start: ";
     cin >> start;
-    if(start == 's'){
+
+    if (start == 's') {
         setup();
-        while(!gameOver){
+        while (!gameOver) {
             draw();
             input();
             logic();
-            Sleep(30);
+            
+            // Dynamic speed adjustment
+            int speed = 100 - (score / 2); // Decrease delay as score increases
+            if (speed < 10) {
+                speed = 10; // Minimum delay
+            }
+            Sleep(speed); 
+            
             system("cls");
         }
+        updateHighScore(score); // Update high score when game ends
     }
 }
 
-void setup(){
+void setup() {
     gameOver = false;
     dir = STOP;
-    headX = width/2;
-    headY = height/2;
-    fruitX = rand()%width;
-    fruitY = rand()%height;
+    headX = width / 2;
+    headY = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
     score = 0;
+    tail_len = 0;
 }
 
-void draw(){
+void draw() {
     system("cls");
-    // Uper Border
+    setColor(14, 0); // Set color for borders and text
+
+    // Upper Border
     cout << "\t\t";
-    for(int i = 0; i < width-8; i++){
+    for (int i = 0; i < width - 8; i++) {
         cout << "||";
     }
     cout << endl;
 
     // Snake, fruit, space and side borders
-    for(int i = 0; i < height; i++){
-        for(int j = 0; j < width; j++){
-            // left border
-            if(j == 0){
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            // Left border
+            if (j == 0) {
                 cout << "\t\t||";
             }
-            // snake head
-            if(i == headY && j == headX){
+            // Snake head
+            if (i == headY && j == headX) {
+                setColor(10, 0); // Set color for snake head
                 cout << "O";
             }
-            // fruit
-            else if(i == fruitY && j == fruitX){
+            // Fruit
+            else if (i == fruitY && j == fruitX) {
+                setColor(12, 0); // Set color for fruit
                 cout << "*";
             }
-            // space, snake tail
-            else{
+            // Space, snake tail
+            else {
                 bool print = false;
-                // tail
-                for(int k = 0; k < tail_len; k++){
-                    if(tailx[k] == j && taily[k] == i){
+                // Tail
+                for (int k = 0; k < tail_len; k++) {
+                    if (tailx[k] == j && taily[k] == i) {
+                        setColor(11, 0); // Set color for snake tail
                         cout << "o";
                         print = true;
                     }
                 }
-                // space
-                if(!print){
+                // Space
+                if (!print) {
                     cout << " ";
                 }
-            } 
-            // right border
-            if(j == width-1){
+            }
+            // Right border
+            if (j == width - 1) {
                 cout << "||";
             }
         }
         cout << endl;
     }
+
     // Lower Border
     cout << "\t\t";
-    for(int i = 0; i < width-8; i++){
+    for (int i = 0; i < width - 8; i++) {
         cout << "||";
     }
     cout << endl;
+
+    setColor(15, 0); // Reset color
     cout << "\t\t\tScore: " << score << endl;
 }
 
-void input(){
-    if(_kbhit())
-    switch (getch())
-    {
-    case 'a':
-        dir = LEFT;
-        break;
-    case 'd':
-        dir = RIGHT;
-        break;
-    case 'w':
-        dir = UP;
-        break;
-    case 's':
-        dir = DOWN;
-        break;    
-    default:
-        break;
+void input() {
+    if (_kbhit()) {
+        switch (getch()) {
+            case 'a':
+                dir = LEFT;
+                break;
+            case 'd':
+                dir = RIGHT;
+                break;
+            case 'w':
+                dir = UP;
+                break;
+            case 's':
+                dir = DOWN;
+                break;
+            default:
+                break;
+        }
     }
 }
 
-void logic(){
-    // tail logic
+void logic() {
+    // Tail logic
     int prevx = tailx[0];
     int prevy = taily[0];
     int prev2x, prev2y;
     tailx[0] = headX;
     taily[0] = headY;
-    for(int i = 1; i < tail_len; i++){
+    for (int i = 1; i < tail_len; i++) {
         prev2x = tailx[i];
         prev2y = taily[i];
         tailx[i] = prevx;
@@ -138,52 +161,67 @@ void logic(){
         prevy = prev2y;
     }
 
-    // direction logic
-    switch (dir)
-    {
-    case LEFT:
-        headX--;
-        break;
-    case RIGHT:
-        headX++;
-        break;
-    case UP:
-        headY--;
-        break;
-    case DOWN:
-        headY++;
-        break;
-    default:
-        break;
+    // Direction logic
+    switch (dir) {
+        case LEFT:
+            headX--;
+            break;
+        case RIGHT:
+            headX++;
+            break;
+        case UP:
+            headY--;
+            break;
+        case DOWN:
+            headY++;
+            break;
+        default:
+            break;
     }
 
-    // touch walls
-    if(headX >= width){
+    // Touch walls
+    if (headX >= width) {
         headX = 0;
-    }
-    else if(headX < 0){
+    } else if (headX < 0) {
         headX = width - 1;
     }
-    if(headY >= height){
+    if (headY >= height) {
         headY = 0;
-    }
-    else if(headY < 0){
+    } else if (headY < 0) {
         headY = height - 1;
     }
 
-    // snake bite itself
-    for(int i = 0; i < tail_len; i++){
-        if(tailx[i] == headX && taily[i] == headY){
+    // Snake bites itself
+    for (int i = 0; i < tail_len; i++) {
+        if (tailx[i] == headX && taily[i] == headY) {
             gameOver = true;
         }
     }
 
-    // snake eat fruit
-    if(headX == fruitX && headY == fruitY){
+    // Snake eats fruit
+    if (headX == fruitX && headY == fruitY) {
         score += 10;
-        fruitX = rand()%width;
-        fruitY = rand()%height;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
         tail_len++;
     }
+}
 
+void setColor(int textColor, int backgroundColor) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, (WORD)((backgroundColor << 4) | textColor));
+}
+
+void updateHighScore(int score) {
+    ifstream infile("highscore.txt");
+    int highScore;
+    infile >> highScore;
+    infile.close();
+
+    if (score > highScore) {
+        ofstream outfile("highscore.txt");
+        outfile << score;
+        outfile.close();
+        cout << "New High Score: " << score << endl;
+    }
 }
